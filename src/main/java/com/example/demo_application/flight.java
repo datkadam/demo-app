cac49f309b01bf109246d1dda3f877e2a5766efd
datkadam <datkadam>
2024-10-09 23:42:00 +0530

    adding flight class/module

---
+++ src/main/java/com/example/demo_application/flight.java
@@ -0,0 +1,11 @@
+package com.example.demo_application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+     @RestController
+    public class flight {
+        @GetMapping("/flight")
+        public String getData() {return "Please book your flight tickets instatnly Around the Globe"; }
+    }
+

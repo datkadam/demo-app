b73a552f8f6cdee71390be4e919b7fdcb89dd57b
datkadam <datkadam>
2024-10-09 21:20:10 +0530

    code refractor

--- src/main/java/com/example/demo_application/hotel.java
+++ src/main/java/com/example/demo_application/hotel.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hotel {
-
     @GetMapping("/hotel")
     public String getData() {return "Please book your bus tickets Online instatnly for Pune Inter City travel"; }
 }
